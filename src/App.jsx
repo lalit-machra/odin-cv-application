2021603146b38fc8e0eda90f243315e4bfce4f45
lalit-machra <lalitmachra0403@gmail.com>
2025-06-11 06:56:45 +0530

    Initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>CV Application</h1>
+      {console.log("hello")}
+    </>
+  )
+}
+
+export default App
